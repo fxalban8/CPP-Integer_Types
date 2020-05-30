b1bb0357016ddbe2225051904ddc82455b63d7f3
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 11:15:17 -0500

    We've printed the maximum value that an int variable can be

--- Integer_Types.cpp
+++ Integer_Types.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <limits>		//we will include the limits library
+#include <limits.h>		//we will include the limits library
 using namespace std;
 
 
 int main(){
 	
+	cout<<"The maximum value of an integer is "<<INT_MAX<<endl;	
 	return 0;
 }
