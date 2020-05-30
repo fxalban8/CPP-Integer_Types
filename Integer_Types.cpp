6fc7fa7f2d6c78ce0da67e864b867208a8b767f0
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 11:21:33 -0500

    We've printed the maximum and minimum value a long variable can store

--- Integer_Types.cpp
+++ Integer_Types.cpp
@@ -9,6 +9,9 @@ int main(){
 	cout<<"The minimum value of an integer is "<<INT_MIN<<"\n"<<endl;
 
 	cout<<"The maximum value of a short int is "<<SHRT_MAX<<endl;
-	cout<<"The minimum value of a short int is "<<SHRT_MIN<<"\n"<<endl;	
+	cout<<"The minimum value of a short int is "<<SHRT_MIN<<"\n"<<endl;
+
+	cout<<"The maximum value of a long value is "<<LONG_MAX<<endl;
+	cout<<"The minimum value of a long value is "<<LONG_MIN<<"\n"<<endl;	
 	return 0;
 }
