92b73bbd15977b8dfac4c5f0b16f24b1d56bc6d4
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 11:16:41 -0500

    We've printed the minimum value that an int variable can be

--- Integer_Types.cpp
+++ Integer_Types.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main(){
 	
-	cout<<"The maximum value of an integer is "<<INT_MAX<<endl;	
+	cout<<"The maximum value of an integer is "<<INT_MAX<<endl;
+	cout<<"The minimum value of an integer is "<<INT_MIN<<endl;	
 	return 0;
 }
